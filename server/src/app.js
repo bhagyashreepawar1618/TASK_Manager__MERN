0e5ce326e63d39d141f57faaf34915ee3457718e
bhagyashreepawar1618 <bhagyashreepawar1618@gmail.com>
2026-04-11 19:34:07 +0530

    added cors origin

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin:
+      process.env.CORS_ORIGIN ||
+      "http://localhost:5173" ||
+      "https://task-manager-mern-zmil.vercel.app/",
     credentials: true,
   }),
 );
